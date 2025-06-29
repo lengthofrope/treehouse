940e246d863e18481650823932e931de077d314a
Bas de Kort <bas.dekort@proudnerds.com>
2025-06-29 13:42:35 +0200

    Remove stub files and CSRF token management script from the project

--- resources/stubs/new-project/config/routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Controllers\HomeController;
-
-// Define your routes here
-$router->get('/', [HomeController::class, 'index']);
-$router->get('/about', [HomeController::class, 'about']);
